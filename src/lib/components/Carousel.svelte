3e9346cbf0aa60eb3a99a82bc9863fd2f2d9cfde
Jakubdonovan <jakubdonovan@protonmail.com>
2022-11-03 06:05:09 +0700

    updated finding winners loading animation

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -18,10 +18,6 @@
 	];
 </script>
 
-<svelte:head>
-	<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-</svelte:head>
-
 <div class="relative w-full flex justify-start items-center">
 	<div class="w-full overflow-x-auto flex md:justify-center gap-6 snap-x snap-mandatory before:shrink-0 before:w-1/3 after:shrink-0 after:w-1/3 md:before:content-none md:after:content-none">
 		{#each slides as { icon, title, desc }, i}
